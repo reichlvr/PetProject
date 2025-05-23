3fd0e04547544dc20cebc49b5218ed2bb308a202
reichlvr <8857497@mail.ru>
2025-05-23 23:32:53 +0300

    Finaly

---
+++ src/corrector/__init__.py
@@ -0,0 +1,9 @@
+from .process_text import process_text  # ключевая строка!
+
+__all__ = [
+    'correct_spelling',
+    'correct_grammar',
+    'remove_repeats',
+    'highlight',
+    'process_text',  # добавьте это
+]
---
+++ src/corrector/process_text.py
@@ -0,0 +1,15 @@
+from .spell import correct_spelling
+from .grammar import correct_grammar
+from .utils import remove_repeats, highlight
+
+def process_text(text: str) -> dict:
+    corrected_grammar, grammar_errors = correct_grammar(text)
+    corrected_spelling = correct_spelling(corrected_grammar)
+    final_text = remove_repeats(corrected_spelling)
+    highlighted = highlight(text, grammar_errors)
+    return {
+        'original': text,
+        'corrected': final_text,
+        'highlighted': highlighted,
+        'errors_count': len(grammar_errors)
+    }
